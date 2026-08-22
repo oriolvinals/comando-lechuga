b0994194781359abcbb3ff49bc2ab3ec5ddd975f
Oriol Viñals <oriolvinals@hotmail.com>
2026-08-22 19:06:49 +0200

    refactor: afegir nous estats al partit

--- app/Enums/FixtureState.php
+++ app/Enums/FixtureState.php
@@ -7,11 +7,17 @@ namespace App\Enums;
 enum FixtureState: string
 {
     case Scheduled = 'scheduled';
+    case FirstHalf = 'first_half';
+    case HalfTime = 'half_time';
+    case SecondHalf = 'second_half';
     case Finished = 'finished';
 
     public static function fromFantasyId(int $stateId): self
     {
         return match ($stateId) {
+            2 => self::FirstHalf,
+            3 => self::HalfTime,
+            4 => self::SecondHalf,
             7 => self::Finished,
             default => self::Scheduled,
         };
