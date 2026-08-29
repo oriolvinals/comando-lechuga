bbcf8df9f85e2d8d984bdd917e9d950ff44fca1d
Oriol Viñals <oriolvinals@hotmail.com>
2026-08-30 00:41:57 +0200

    refactor: treure tots els seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,26 +5,11 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
         $this->call([SeasonSeeder::class]);
-
-        Artisan::call('season:sync-week');
-        Artisan::call('season:sync-teams');
-        Artisan::call('season:sync-fixtures');
-        Artisan::call('season:sync-players');
-        Artisan::call('season:sync-player-photos');
-        Artisan::call('season:sync-player-markets');
-        Artisan::call('season:sync-player-scores');
-        Artisan::call('season:sync-player-score-stats');
-        Artisan::call('season:sync-market');
-        Artisan::call('season:sync-standing');
-        Artisan::call('season:sync-manager-lineups');
-        Artisan::call('season:sync-activity');
-        Artisan::call('season:sync-manager-players');
     }
 }
